3c37e2fc06c8c86cb90977a5461c1cd348436cfe
Naura Azarine <nauraazarine@gmail.com>
2025-02-13 23:42:02 +0700

    repair

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,25 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
-
-class YourController extends Controller
-{
-    public function fetchData()
-    {
-        // Panggil API dari InfinityFree
-        $response = Http::get("http://knimoni.infy.uk/db-api.php");
-        
-        // Ubah response menjadi array
-        $data = $response->json();
-
-        // Kirim data ke view
-        return view('index', ['data' => $data]);
-    }
-}
+#app\Http\Controllers\ChildController.php
